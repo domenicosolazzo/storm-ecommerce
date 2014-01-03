34e47a17ae16d4849d6bc584e135f0e152d25cea
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-01-03 14:05:00 +0100

    Added NavigationEntry

---
+++ src/main/java/storm/analytics/utilities/NavigationEntry.java
@@ -0,0 +1,11 @@
+package storm.analytics.utilities;
+
+import java.io.Serializable;
+
+/**
+ * User: domenicosolazzo
+ */
+public class NavigationEntry implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+}

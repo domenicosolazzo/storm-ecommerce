46f3776eee19b7832fef39d6ce071ecf6d22eefb
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-01-03 17:30:02 +0100

    Updated Category bolt

--- src/main/java/storm/analytics/GetCategoryBolt.java
+++ src/main/java/storm/analytics/GetCategoryBolt.java
@@ -6,6 +6,7 @@ import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 import storm.analytics.utilities.NavigationEntry;
@@ -51,5 +52,10 @@ public class GetCategoryBolt extends BaseBasicBolt {
                 ex.printStackTrace();
             }
         }
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        outputFieldsDeclarer.declare(new Fields("user", "product", "categ"));
     }
 }

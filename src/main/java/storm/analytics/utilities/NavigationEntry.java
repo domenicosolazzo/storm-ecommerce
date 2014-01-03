6a301e67ae9450a3605762d666651d8611954e60
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-01-03 14:20:01 +0100

    Updated NavigationEntry

--- src/main/java/storm/analytics/utilities/NavigationEntry.java
+++ src/main/java/storm/analytics/utilities/NavigationEntry.java
@@ -1,6 +1,7 @@
 package storm.analytics.utilities;
 
 import java.io.Serializable;
+import java.util.Map;
 
 /**
  * User: domenicosolazzo
@@ -8,4 +9,51 @@ import java.io.Serializable;
 public class NavigationEntry implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    String userId;
+    String pageType;
+    @SuppressWarnings("rawtypes")
+    Map otherData;
+
+    @SuppressWarnings("rawtypes")
+    public NavigationEntry(String userId, String pageType, Map otherData) {
+        this.userId = userId;
+        this.pageType = pageType;
+        this.otherData= otherData;
+    }
+
+    public String getUserId(){
+        return this.userId;
+    }
+
+    public void setUserId(String userId){
+        this.userId = userId;
+    }
+
+    public String getPageType(){
+        return this.pageType;
+    }
+
+    public void setPageType(String pageType){
+        this.pageType = pageType;
+    }
+
+    @SuppressWarnings("rawtypes")
+    public Map getOtherData(){
+        return this.otherData;
+    }
+
+    @SuppressWarnings("rawtypes")
+    public void setOtherData(Map otherData){
+        this.otherData = otherData;
+    }
+
+    public String toString(){
+        String ret = "User:" + userId + " navigating a " + pageType;
+        if(otherData!=null){
+            ret += " page with "+otherData.toString();
+        }
+        return ret;
+
+    }
 }
